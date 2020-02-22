fbe4b7f789666f99b64ca804aedb4c1e5a4de376
kmhayeon <kmhayeon@naver.com>
2020-02-22 16:46:06 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import AddNumberRoot from "./component/AddNumberRoot";
 import DisplayNumberRoot from "./component/DisplayNumberRoot";
 
 
-
-function App() {
-    return(
-        <div className="App">
-            <h1>Root</h1>
-            <AddNumberRoot></AddNumberRoot>
-            <DisplayNumberRoot></DisplayNumberRoot>
-        </div>
-    );
+class App extends Component {
+    state={number:0}
+    render() {
+        return (
+            <div className="App">
+                <h1>Root</h1>
+                <AddNumberRoot onClick={function (size) {
+                    this.setState({number:this.state.number + size});
+                }.bind(this)}></AddNumberRoot>
+                <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
+            </div>
+        );
+    }
 }
-
 
 
 
